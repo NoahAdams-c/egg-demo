db5b587b7a97b7cbe9b3b88ee1de1713edaf7f64
chenchen <chenchen_9694@outlook.com>
2020-01-07 15:30:21 +0800

    字段名错误修改

--- app/io/middleware/auth.js
+++ app/io/middleware/auth.js
@@ -3,7 +3,7 @@
  * @Author: chenchen
  * @Date: 2019-10-23 15:04:43
  * @LastEditors: chenchen
- * @LastEditTime: 2020-01-07 15:28:24
+ * @LastEditTime: 2020-01-07 15:30:06
  */
 module.exports = app => {
 	return async (ctx, next) => {
@@ -11,14 +11,14 @@ module.exports = app => {
 		let { userInfo } = ctx.socket.handshake.query
 		console.log(userInfo)
 		userInfo = JSON.parse(userInfo)
-		if (userInfo.userId) {
-			ctx.app.socketIdMaps[userInfo.userId] = {
+		if (userInfo.user_id) {
+			ctx.app.socketIdMaps[userInfo.user_id] = {
 				socketId,
 				nickName: userInfo.nickName,
 				avatar: userInfo.avatar
 			}
 			// 连接成功提示客户端
-			console.log(ctx.app.socketIdMaps[userInfo.userId], "已连接")
+			console.log(ctx.app.socketIdMaps[userInfo.user_id], "已连接")
 			ctx.socket.emit("connected")
 			ctx.helper.logOnlineUsers()
 		} else {
@@ -26,8 +26,8 @@ module.exports = app => {
 		}
 		await next()
 		// 断开连接
-		console.log(ctx.app.socketIdMaps[userInfo.userId], "连接已断开")
-		delete ctx.app.socketIdMaps[userInfo.userId]
+		console.log(ctx.app.socketIdMaps[userInfo.user_id], "连接已断开")
+		delete ctx.app.socketIdMaps[userInfo.user_id]
 		ctx.helper.logOnlineUsers()
 	}
 }
